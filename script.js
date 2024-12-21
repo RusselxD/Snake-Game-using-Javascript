d0ef61f1a8377c2910dd94e157600cb40771be11
RusselxD <russelcabigquez8@gmail.com>
2024-12-21 11:16:14 +0800

    started dynamic canvas resizing

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
-console.log(ctx);
-
 let currentDirection = "ArrowRight";
 let directionChanged = false;
 
+var snakeSize = 15;
+
 let snake = [
     { x: 150, y: 150 },
-    { x: 140, y: 150 },
-    { x: 130, y: 150 },
+    { x: 135, y: 150 },
     { x: 120, y: 150 },
-    { x: 110, y: 150 },
-    { x: 100, y: 150 },
-    { x: 90, y: 150 },
-    { x: 80, y: 150 },
+    { x: 105, y: 150 },
+    // { x: 110, y: 150 },
+    // { x: 100, y: 150 },
+    // { x: 90, y: 150 },
+    // { x: 80, y: 150 },
 ];
 
 const directions = {
-    ArrowUp: { x: 0, y: -10 },
-    ArrowDown: { x: 0, y: 10 },
-    ArrowRight: { x: 10, y: 0 },
-    ArrowLeft: { x: -10, y: 0 },
+    ArrowUp: { x: 0, y: -snakeSize },
+    ArrowDown: { x: 0, y: snakeSize },
+    ArrowRight: { x: snakeSize, y: 0 },
+    ArrowLeft: { x: -snakeSize, y: 0 },
 };
 
 let foodX = 0;
 let foodY = 0;
+
+
 
 document.body.addEventListener("keydown", (event) => changeDirection(event.key));
 addFood();
@@ -33,18 +35,22 @@ snake.forEach((part) => drawSnakePart(part));
 
 function getRandomCoords() {
     do {
-        var x = Math.floor(Math.random() * canvas.width - 9);
-        var y = Math.floor(Math.random() * canvas.height - 9);
+        var x = Math.floor(Math.random() * (canvas.width - (snakeSize - 1)));
+        var y = Math.floor(Math.random() * (canvas.height - (snakeSize - 1)));
 
-        x = x - (x % 10);
-        y = y - (y % 10);
+        console.log(x)
+       
+        x = x - (x % snakeSize);
+        y = y - (y % snakeSize);
 
         var coordIsInSnake = false;
-        snake.forEach((part) => {
+        for(let part of snake){
             if (part.x === x && part.y === y) {
                 coordIsInSnake = true;
+                break;
             }
-        });
+        }
+        
         // this ensures that the generate coords
         // are not within the body of the snake
     } while (coordIsInSnake);
@@ -59,8 +65,8 @@ function addFood() {
     // painting food
     ctx.strokeStyle = "rgb(197, 13, 56)";
     ctx.fillStyle = "rgb(217, 40, 81)";
-    ctx.fillRect(foodX, foodY, 10, 10);
-    ctx.strokeRect(foodX, foodY, 10, 10);
+    ctx.fillRect(foodX, foodY, snakeSize, snakeSize);
+    ctx.strokeRect(foodX, foodY, snakeSize, snakeSize);
 }
 
 const changeDirection = (key) => {
@@ -72,19 +78,25 @@ const changeDirection = (key) => {
 
 function drawSnakePart({ x, y }) {
     ctx.fillStyle = "rgb(48, 246, 71)";
-    ctx.strokeStyle = "rgb(0, 0, 0)";
 
-    ctx.fillRect(x, y, 10, 10);
-    ctx.strokeRect(x, y, 10, 10);
+    ctx.fillRect(x, y, snakeSize, snakeSize);
+    ctx.strokeRect(x, y, snakeSize, snakeSize);
 }
 
 function drawSnake() {
     // erase tail
     const tail = snake[snake.length - 1];
-    ctx.clearRect(tail.x - 1, tail.y - 1, 12, 12);
+    ctx.clearRect(tail.x - 1, tail.y - 1, snakeSize + 2, snakeSize + 2 );
     snake.pop();
 
-    snake.forEach((part) => drawSnakePart(part));
+    ctx.fillStyle = "rgb(12, 206, 227)";
+
+    ctx.fillRect(snake[0].x, snake[0].y, snakeSize, snakeSize);
+    ctx.strokeRect(snake[0].x, snake[0].y, snakeSize, snakeSize);
+
+    for(let i = 1; i < snake.length; i++){
+        drawSnakePart(snake[i]);
+    }
 
     // if food is eaten
     if (snake[0].x === foodX && snake[0].y === foodY) {
@@ -107,11 +119,21 @@ const moveSnake = () => {
         y: snake[0].y + directions[currentDirection].y,
     };
 
+    console.log(head.x, head.y);
+
+    ctx.strokeStyle = "rgb(0, 0, 0)";
+
     if (collided(head)) {
         clearInterval(moving);
         document.body.removeEventListener("keydown", (event) =>
             changeDirection(event.key)
         );
+
+        ctx.fillStyle = "rgb(231, 22, 22)";
+
+        ctx.fillRect(snake[0].x, snake[0].y, snakeSize, snakeSize);
+        ctx.strokeRect(snake[0].x, snake[0].y, snakeSize, snakeSize);
+
         return;
     }
 
@@ -133,18 +155,18 @@ function keyIsOppositeDirection(key) {
 }
 
 function collided({ x, y }) {
-    // check if collided with body
+    // check if head collided with body
     for (let i = 1; i < snake.length; i++) {
         if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
             return true;
         }
     }
 
-    // check if collided with walls
-    return y <= -10 || y >= canvas.height || x <= -10 || x >= canvas.width;
+    // check if head collided with walls
+    return y <= -snakeSize || y >= canvas.height || x <= -snakeSize || x >= canvas.width;
 }
 
-const moving = setInterval(moveSnake, 100);
-// easy - 70
-// average - 55
-// hard - 40
+const moving = setInterval(moveSnake, 70);
+// easy - 80
+// average - 70
+// hard - 55
